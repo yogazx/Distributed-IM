8af2a160aebebcf70a2631f2739a979377ae3884
geyoujia <youjiage1995@gmail.com>
2020-01-13 21:31:42 +0800

    初始化项目

---
+++ Distributed-IM-Route/src/test/java/com/yoge/distributed/im/route/RouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoge.distributed.im.route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
